87ab70e999447c28982ebc41ab2ea66356f102b9
wzz123456 <vvzz_run@outlook.com>
2018-06-29 17:33:51 +0800

    继续完善接口成员函数

--- binsearchtree.h
+++ binsearchtree.h
@@ -1,32 +1,44 @@
 #ifndef DATASTRUCTURE_ALGORITHM_LIBRARY_H
 #define DATASTRUCTURE_ALGORITHM_LIBRARY_H
 
-template <typename Comparable>
-class BinarySeachTree{
+template< typename Comparable >
+class BinarySeachTree {
 public:
-    BinarySeachTree();
-    BinarySeachTree(const BinarySeachTree &rhs);
-    ~BinarySeachTree();
+	BinarySeachTree();
+	BinarySeachTree( const BinarySeachTree &rhs );
+	~BinarySeachTree();
+	const Comparable &findMin() const;
+	const Comparable &findMax() const;
+	bool contains( const Comparable &x ) const;
+	bool isEmpty() const;
+	void printTree() const;
+	
+	void makeEmpty();
+	void insert( const Comparable &x );
+	void remove( const Comparable &x );
+	
+	const BinarySeachTree &operator=( const Comparable &rhs );
 
-    bool contains(const Comparable &x) const ;
-    const Comparable & findMin() const ;
-    const Comparable & findMax() const ;
 private:
-    struct BinaryNode{
-        Comparable element;
-        BinaryNode *lchild;
-        BinaryNode *rchild;
-
-        BinaryNode(const Comparable &value,BinaryNode *l,BinaryNode *r):
-                element(value),lchild(l),rchild(r) { }
-    };
-
-    BinaryNode *root;
-
-    bool contains(const Comparable &x,BinaryNode *t) const ;
-    BinaryNode *findMin(BinaryNode *t) const ;
-    BinaryNode *findMax(BinaryNode *t) const ;
-
+	struct BinaryNode {
+		Comparable element;
+		BinaryNode *lchild;
+		BinaryNode *rchild;
+		
+		BinaryNode( const Comparable &value, BinaryNode *l, BinaryNode *r ) :
+				element( value ), lchild( l ), rchild( r ) {}
+	};
+	
+	BinaryNode *root;
+	
+	void insert( const Comparable &x, BinaryNode *&t ) const;
+	void remove( const Comparable &x, BinaryNode *&t ) const;
+	BinaryNode *findMin( BinaryNode *t ) const;
+	BinaryNode *findMax( BinaryNode *t ) const;
+	bool contains( const Comparable &x, BinaryNode *t ) const;
+	void makeEmpty( BinaryNode *&t );
+	void printTree( BinaryNode *t );
+	BinaryNode *clone( BinaryNode *t ) const;
 };
 
 #endif
